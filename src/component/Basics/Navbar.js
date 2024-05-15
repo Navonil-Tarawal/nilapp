57e10a92148c173021a78647852b157c1da20ee2
Navonil Tarawal <navonil.tarawal>
2024-05-15 16:29:30 +0530

    app name changed

--- src/component/Basics/Navbar.js
+++ src/component/Basics/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">nilApp</a>
+    <a class="navbar-brand" href="#">FoodyPoP</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
